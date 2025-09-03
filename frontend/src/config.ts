0d2765480f5c80aaf5edf84b7f9f176f100dca5a
Jonahas <jonas.h.faber@gmail.com>
2025-09-04 01:22:53 +0200

    remove trailing slash

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "production" ? 'https://api-velopass-todo.faber.zip/' : 'https://localhost:7115';
+export const API_URL = process.env.NODE_ENV === "production" ? 'https://api-velopass-todo.faber.zip' : 'https://localhost:7115';
